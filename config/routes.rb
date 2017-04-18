ec70c85091fa79f0728fe9330df81dd19ac54f62
ethankhoa <ethan.huynh@gmail.com>
2017-04-18 12:34:17 +1000

    fixes omniauth facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   authenticated :user do
     root 'pages#dashboard', as: :authenticated_root
