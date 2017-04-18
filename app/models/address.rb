ce15862cfb7de878af89a7802704c102db5fbd3e
ethankhoa <ethan.huynh@gmail.com>
2017-04-18 15:52:12 +1000

    adds address table

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,3 @@
 class Address < ApplicationRecord
-  belongs_to :user
-  belongs_to :org
+  belongs_to :addressable, :polymorphic => true
 end
--- app/models/org.rb
+++ app/models/org.rb
@@ -1,3 +1,4 @@
 class Org < ApplicationRecord
   belongs_to :user
+  has_one :address, :as => :addressable
 end
