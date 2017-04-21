f5d42049e8e5c7c38209bc1af7b008bb2c865bb9
ethankhoa <ethan.huynh@gmail.com>
2017-04-21 17:58:29 +1000

    updated app controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def address_check
+    @user.address.line_1.nil?
+  end
+
+  def phone_check
+    @user.address.telephone.nil?
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :addressable, :polymorphic => true
 end
-
-def address_check
-  current_user.address.line_1.nil?
-end
-
-def phone_check
-  current_user.address.telephone.nil?
-end
