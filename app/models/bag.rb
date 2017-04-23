08819cab8a12ddda55a9bac63b650dac6cf0d7aa
ethankhoa <ethan.huynh@gmail.com>
2017-04-24 01:15:39 +1000

    adds itemstotal method to bag model

--- app/models/bag.rb
+++ app/models/bag.rb
@@ -1,3 +1,22 @@
 class Bag < ApplicationRecord
   belongs_to :user
+  belongs_to :bag_status
+
+  has_many :bag_items
+  before_create :set_order_status
+  before_save :update_itemtotal
+
+  def itemtotal
+    bag_items.collect { |bi| bi.valid? ? (bi.quantity) : 0 }.sum
+  end
+
+  private
+  def set_order_status
+    self.order_status_id = 1
+  end
+
+  def update_itemtotal
+  self[:bagtotal] = total_items
+  end
+
 end
