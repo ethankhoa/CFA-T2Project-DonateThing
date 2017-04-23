ebfefd7204fe2d61c6befd9319a3fc97e6eff5a5
ethankhoa <ethan.huynh@gmail.com>
2017-04-24 01:33:42 +1000

    adds bagitems controller

--- app/controllers/bag_items_controller.rb
+++ app/controllers/bag_items_controller.rb
@@ -1,10 +1,27 @@
 class BagItemsController < ApplicationController
   def create
+    @bag = current_bag
+    @bag_item = @bag.bag_items.new(bag_item_params)
+    @bag.save
+    session[:bag_id] = @bag.id
   end
 
   def update
+    @bag = current_bag
+    @bag_item = @bag.bag_items.find(params[:id])
+    @bag_item.update_attributes(bag_item_params)
+    @bag_items = @bag.bag_items
   end
 
   def destroy
+    @bag = current_bag
+    @bag_item = @bag.bag_items.find(params[:id])
+    @bag_item.destroy
+    @bag_items = @bag.bag_items
+  end
+
+  private
+  def bag_item_params
+    params.require(:bag_item).permit(:quantity)
   end
 end
